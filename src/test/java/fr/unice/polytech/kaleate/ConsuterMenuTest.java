d175c250e87d96d14fc75483c2e9a69a06387645
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2023-10-03 17:22:31 +0200

    feature : consulter les menus avec cucumber #1

--- src/test/java/fr/unice/polytech/kaleate/ConsuterMenuTest.java
+++ src/test/java/fr/unice/polytech/kaleate/ConsuterMenuTest.java
@@ -17,6 +17,20 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.unice.polytech.kaleate")
 
-public class RunCucumberTest {
+public class ConsuterMenuTest {
+    @Given("je suis connecte avec mon compte etudiant")
+    public void je_suis_connecte_avec_mon_compte_etudiant() {
+        // Write code here that turns the phrase above into concrete actions
+        
+    }
+    @When("j'ecris {string} dans le terminal")
+    public void j_ecris_dans_le_terminal(String string) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("je vois la liste des menus disponibles pour mon campus")
+    public void je_vois_la_liste_des_menus_disponibles_pour_mon_campus() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
 
 }
