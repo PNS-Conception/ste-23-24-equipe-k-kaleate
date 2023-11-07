6b00321e6aee96df68d8d755a061f6baa32e7f5c
RomainPellegrini <romain.pellegrini@etu.unice.fr>
2023-11-07 15:46:21 +0100

    fix : bug #38

--- src/test/java/fr/unice/polytech/kaleate/UserStory3Test.java
+++ src/test/java/fr/unice/polytech/kaleate/UserStory3Test.java
@@ -1,14 +1,25 @@
 package fr.unice.polytech.kaleate;
 
 import io.cucumber.java.fr.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static org.junit.Assert.*;
 
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.unice.polytech.kaleate")
 
 public class UserStory3Test {
     static Utilisateur userA = new Utilisateur("User","A");
@@ -143,8 +154,12 @@ public class UserStory3Test {
     @Etantdonné("User C qui rejoint la commande groupée")
     public void user_c_qui_rejoint_la_commande_groupée() {
         // Write code here that turns the phrase above into concrete actions
+        commandeMemeCreneau();
+        commandeUserA = new CommandeGroupee(commandeUserA);
+        assertEquals(userA,commandeUserA.getUtilisateur());
+        commandeGroupee = (CommandeGroupee) commandeUserA;
         int size = commandeGroupee.getCommandes().size();
-        userC.rejoindreCommandegroupee(commandeGroupee,commandeGroupee.getCode());
+        assertTrue(userC.rejoindreCommandegroupee(commandeGroupee,commandeGroupee.getCode()));
         assertEquals(size+1,commandeGroupee.getCommandes().size());
     }
     @Quand("User C affiche la liste des menus")
