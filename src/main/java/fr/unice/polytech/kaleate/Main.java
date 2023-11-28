9040c3d02a83e73ece67aecf3af16967077da35a
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2023-11-28 16:51:17 +0100

    sonarqube fix #63

--- src/main/java/fr/unice/polytech/kaleate/Main.java
+++ src/main/java/fr/unice/polytech/kaleate/Main.java
@@ -12,50 +12,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        /*Utilisateur user = new Utilisateur("com","mande");
-        ArrayList<Menu> menus = new ArrayList<>();
-        menus.add(new Menu(1, "cov", new Creneau(new Date(), new Date())));
-        menus.add(new Menu(2, "ide", new Creneau(new Date(), new Date())));
-        menus.add(new Menu(3, "ad", new Creneau(new Date(), new Date())));
-        Commande c = new Commande(user, menus, new Restaurant("rest"));
-        c.valideeCommande();
-        System.out.println(c.getStatut());
-        menus.get(0).setStatutEnPreparation();
-        System.out.println(c.getStatut());
-        menus.get(0).setStatutPret();
-        System.out.println(c.getStatut());
-        menus.get(1).setStatutPret();
-        System.out.println(c.getStatut());
-        menus.get(2).setStatutPret();
-        System.out.println(c.getStatut());*/
-
-        Commande c = new CommandeSimple();
-
-        ListeMenus menus = new ListeMenus();
-        menus.add(new Menu(1, "cov", new Creneau(new Date(), new Date())));
-        menus.add(new Menu(2, "ide", new Creneau(new Date(), new Date())));
-        menus.add(new Menu(3, "ad", new Creneau(new Date(), new Date())));
-
-        for (Menu m : menus) {
-            c.addMenu(m);
-        }
-
-        c.valideeCommande();
-        c.enregistrerCommande();
-
-        System.out.println("commande " + c.getStatut());
-
-        for (Menu m : menus) {
-            System.out.println("menu " + m.getStatut());
-            m.setStatutEnPreparation();
-            System.out.println("menu " + m.getStatut());
-            System.out.println("commande " + c.getStatut());
-        }
-
-        for(Menu m : menus){
-            m.setStatutPret();
-            System.out.println("menu " + m.getStatut());
-            System.out.println("commande " + c.getStatut());
-        }
+        
     }
 }
