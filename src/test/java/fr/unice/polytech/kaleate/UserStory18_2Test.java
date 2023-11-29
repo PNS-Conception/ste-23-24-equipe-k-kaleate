92677f66b503b79ccdad26bcb42615cbbd8e71b1
CarlaWagschal1 <91202458+CarlaWagschal1@users.noreply.github.com>
2023-11-29 16:26:30 +0100

    #76 initialisation

---
+++ src/test/java/fr/unice/polytech/kaleate/UserStory18_2Test.java
@@ -0,0 +1,113 @@
+package fr.unice.polytech.kaleate;
+
+import fr.unice.polytech.kaleate.menu.BuilderContenuMenu;
+import fr.unice.polytech.kaleate.menu.ContenuMenu;
+import fr.unice.polytech.kaleate.menu.Menu;
+import fr.unice.polytech.kaleate.menu.composant.BuilderChoixComposant;
+import fr.unice.polytech.kaleate.menu.composant.ChoixComposant;
+import fr.unice.polytech.kaleate.menu.composant.Composant;
+import fr.unice.polytech.kaleate.menu.element.BuilderChoixElement;
+import fr.unice.polytech.kaleate.menu.element.BuilderElement;
+import fr.unice.polytech.kaleate.menu.element.ChoixElement;
+import fr.unice.polytech.kaleate.menu.element.Element;
+import fr.unice.polytech.kaleate.outils.Creneau;
+import fr.unice.polytech.kaleate.restaurant.ManagerRestaurant;
+import fr.unice.polytech.kaleate.restaurant.Restaurant;
+import io.cucumber.java.fr.Alors;
+import io.cucumber.java.fr.Etantdonnéque;
+import io.cucumber.java.fr.Quand;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserStory18_2Test {
+    static Creneau creneau = new Creneau(new Date(), new Date());
+
+    static Restaurant restaurant;
+    static ManagerRestaurant managerRestaurant;
+
+    static Composant painBrioche, steak, cheddar, emmental, tomate, mozza;
+    static Composant chocolat, vanille, fraise;
+
+    static ChoixComposant pain, viande, fromage;
+    static ChoixComposant compositionSalade;
+    static ChoixComposant parfum;
+
+    static Element burger, salade, glace;
+
+    static ChoixElement plat, dessert;
+
+    static ContenuMenu contenuMenu;
+
+    static Menu menu;
+
+    public static void creationDesComposant(){
+        painBrioche = new Composant("Pain Brioche");
+        steak = new Composant("Steak");
+        cheddar = new Composant("Cheddar");
+        emmental = new Composant("Emmental");
+        tomate = new Composant("Tomate");
+        mozza = new Composant("Mozza");
+        chocolat = new Composant("Chocolat");
+        vanille = new Composant("Vanille");
+        fraise = new Composant("Fraise");
+    }
+
+    public static void creationDesChoixComposant(){
+        pain = new BuilderChoixComposant().newChoix("Pain", 1, List.of(painBrioche));
+        viande = new BuilderChoixComposant().newChoix("Viande", 1, List.of(steak));
+        fromage = new BuilderChoixComposant().newChoix("Fromage", 1, List.of(cheddar, emmental));
+        compositionSalade = new BuilderChoixComposant().newChoix("Composition Salade", 2, List.of(tomate, mozza));
+        parfum = new BuilderChoixComposant().newChoix("Parfum", 3, List.of(chocolat, vanille, fraise));
+    }
+
+    public static void creationDesElement(){
+        BuilderElement builderElementBurger = new BuilderElement("Burger Cheese");
+        builderElementBurger.addChoixComposant(pain);
+        builderElementBurger.addChoixComposant(viande);
+        builderElementBurger.addChoixComposant(fromage);
+        burger = builderElementBurger.getResult();
+        BuilderElement builderElementSalade = new BuilderElement("Salade");
+        builderElementSalade.addChoixComposant(compositionSalade);
+        salade = builderElementSalade.getResult();
+        BuilderElement builderElementGlace = new BuilderElement("Glace");
+        builderElementGlace.addChoixComposant(parfum);
+        glace = builderElementGlace.getResult();
+    }
+
+    public static void creationChoixElement(){
+        plat = new BuilderChoixElement().newChoix("Plat", 1, List.of(burger, salade));
+        dessert = new BuilderChoixElement().newChoix("Dessert", 1, List.of(glace));
+    }
+
+
+    public static void creationContenuMenu(){
+        BuilderContenuMenu builderContenuMenu = new BuilderContenuMenu();
+        builderContenuMenu.addChoixElement(plat);
+        builderContenuMenu.addChoixElement(dessert);
+        contenuMenu = builderContenuMenu.getResult();
+    }
+
+    public static void creerMenu(){
+        menu = new Menu(12,"menu1", creneau);
+        menu.setContenuMenu(contenuMenu);
+    }
+
+    public static void creationRestaurant(){
+        creationDesComposant();
+        creationDesChoixComposant();
+        creationDesElement();
+        creationChoixElement();
+        creationContenuMenu();
+        creerMenu();
+        restaurant = new Restaurant("restaurant");
+        restaurant.ajouterMenu(menu);
+    }
+
+
+
+
+
+
+}
