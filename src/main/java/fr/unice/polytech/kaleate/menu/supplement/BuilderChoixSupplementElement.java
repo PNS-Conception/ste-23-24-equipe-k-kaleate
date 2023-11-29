e2b1baf3e80fcb3d9f204865190c6536285214c8
CarlaWagschal1 <91202458+CarlaWagschal1@users.noreply.github.com>
2023-11-29 17:47:51 +0100

    #76 oubbli du constructeur

--- src/main/java/fr/unice/polytech/kaleate/menu/supplement/BuilderChoixSupplementElement.java
+++ src/main/java/fr/unice/polytech/kaleate/menu/supplement/BuilderChoixSupplementElement.java
@@ -5,6 +5,10 @@ import fr.unice.polytech.kaleate.menu.element.SupplementElement;
 
 public class BuilderChoixSupplementElement extends BuilderChoixSupplement<SupplementElement> {
 
+        public BuilderChoixSupplementElement() {
+            this.choixSupplement = new ChoixSupplementElement();
+        }
+
         @Override
         public void reset() {
             this.choixSupplement = new ChoixSupplementElement();
